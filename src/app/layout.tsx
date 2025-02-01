ec60e80844fe705b0a29a393e57b48b5af3c62dd
Dayve0 <104338781+Dayve0@users.noreply.github.com>
2025-02-01 15:39:32 -0300

    Feat: Vercel insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "@/config/ThemeContext";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       <html lang="pt-BR">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           {children}
+          <SpeedInsights />
         </body>
       </html>
     </ThemeProvider>
